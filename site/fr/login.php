167e070097b73bf83ad8c49358f7290b624f96d2
Kwyvo <157235784+Kwyvo@users.noreply.github.com>
2025-06-11 23:41:42 +0200

    Update login.php

--- site/fr/login.php
+++ site/fr/login.php
@@ -9,9 +9,9 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="all.css">
+    <link rel="stylesheet" href="css/all.css">
     <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="color_var.css">
+    <link rel="stylesheet" href="css/color_var.css">
 
 </head>
 
